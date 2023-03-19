53c7af66455407d46a12ee414970c1ad0de215d2
Diti <125037089+diti8765@users.noreply.github.com>
2023-03-19 15:45:25 +0530

    Update new script.js

--- new script.js
+++ new script.js
@@ -74,6 +74,134 @@ c=i;
 }
 screen.value=c;
     }
+
+ function hcf(){
+var a=parseInt(document.getElementById("screen1").value),b=parseInt(document.getElementById("screen2").value),gcd;
+for(i=1;i<=(a<b?a:b);i++){
+if(a%i==0&b%i==0)
+gcd=i
+}
+screen.value=gcd;
+    }
+
+    function lcm(){
+var a=parseInt(document.getElementById("screen1").value),b=parseInt(document.getElementById("screen2").value);
+for(i=(a>b?a:b);i<=a*b;i+=(a>b?a:b)){
+if(i%a==0&&i%b==0){
+break;
+}
+}
+screen.value=i;
+    }
+    
+ function octtodec(){
+var a=screen.value,k=0,b=0;
+while(a){
+b=b+a%10*(8**k);
+k++;
+a=parseInt(a/10);
+}
+screen.value=b;
+ }
+
+ function bintodec(){
+var a=screen.value,k=0,b=0;
+if(btntext==1||btntext==0){
+while(a){
+b=b+a%10*(2**k);
+k++;
+a=parseInt(a/10);
+}
+screen.value=b;
+ }
+else{
+alert("reenter it is not valid");
+clrdisp();
+}
+}
+
+ function dectobin(){
+var a=screen.value,k=0,b=0;
+while(a){
+b=b+a%2*(10**k);
+k++;
+a=parseInt(a/2);
+}
+screen.value=b;
+ }
+
+ function fibbo(){
+var a=-1,b=1,c,n=screen.value,k=0;
+while(k<=n){
+c=a+b;
+a=b;b=c;
+k++;
+}
+screen.value=c;
+ }
+
+ function ramanujannum(){
+var a=screen.value,t=0;
+for(i=1729;t<a;i++){
+k=0;
+for(x=1;x**3<i;x++){
+for(y=x+1;x**3+y**3<=i;y++){
+    if(x**3+y**3==i)
+    k++;
+}
+}
+if(k==2){
+screen.value=i;
+t++;
+}
+}
+ }
+    
+ function neilarmsnum(){
+var a=screen.value,t=0;
+for(i=1;t<a;i++){
+nod=i.toString().length;
+sum=0;
+temp=i;
+while(temp){
+b=temp%10;
+sum+=b**nod;
+temp=parseInt(temp/10);
+}
+if(sum==i){
+t++;
+screen.value=i;
+}
+}
+ }
+
+ function isprime(){
+var a=parseInt(screen.value);
+for(i=2;i<=a;i++){
+if(a%i==0)
+break;
+}
+if(a==i)
+screen.value="prime number";
+else
+screen.value="not prime number";
+ } 
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
     
   //dekhlo yaha se hai  
 /*<!DOCTYPE html>
