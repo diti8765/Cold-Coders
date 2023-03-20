4d3522f9502e8353ca6e99a00e3f77ebad410e86
Tauqeer52521 <127301063+Tauqeer52521@users.noreply.github.com>
2023-03-20 18:37:29 +0530

    Update new script.js

--- new script.js
+++ new script.js
@@ -1,4 +1,5 @@
 //to function the buttons
+function effect(){
 var screen=document.querySelector('#screen');
        var btn=document.querySelectorAll('.btn');
   for(item of btn)
@@ -16,58 +17,63 @@ var screen=document.querySelector('#screen');
        screen.value+=btntext;
     });
 }
+}
 
     function sin()
     {
-     screen.value=Math.sin(screen.value);
+           var a=document.getElementById("screen").value;
+    document.getElementById("screen").value =Math.sin(a);
     }
     function cos()
     {
-     screen.value=Math.cos(screen.value);
+           var a=document.getElementById("screen").value;
+     document.getElementById("screen").value=Math.cos(a);
     }
     function tan()
     {
-     screen.value=Math.tan(screen.value);
+           var a=document.getElementById("screen").value;
+     document.getElementById("screen").value=Math.tan(a);
     }
     // a no. raise to the power other
     function pow()
     {
            var a=parseFloat(document.getElementById("screen1").value),b=parseFloat(document.getElementById("screen2").value);
-     screen.value=a**b;
+     document.getElementById("screen").value=a**b;
     }
 //square of a number
     function pow()
     {
-           var a=screen.value;
-     screen.value=a**2;
+           var a=document.getElementById("screen").value;
+     document.getElementById("screen").value=a**2;
     }
 //square root of any number
     function sqrt()
     {
-           var a=screen.value;
-     screen.value=a**0.5;
+           var a=document.getElementById("screen").value;
+     document.getElementById("screen").value=a**0.5;
     }
     function log()
     {
-     screen.value=Math.log(screen.value);
+           var a=document.getElementById("screen").value;
+     document.getElementById("screen").value=Math.log(a);
     }
     function e()
     {
-     screen.value= 2.718281828459045;
+     document.getElementById("screen").value= 2.718281828459045;
     }
 //factorial of a no.
     function fact()
     {
-        var a=screen.value;
+        var a=document.getElementById("screen").value;
            if(a==0)
-                 screen.value=1; 
+                 document.getElementById("screen").value=1; 
         if(a>0){
         while(a)
         {
             f=f*a;
                a--;
         }
-     screen.value=f;
+     document.getElementById("screen").value=f;
         }
     if(a<0)
            alert("factorial is not defined");
@@ -75,7 +81,7 @@ var screen=document.querySelector('#screen');
 //delete input one by one
     function backspc()
     {
-     screen.value=screen.value.substr(0,screen.value.length-1);
+     document.getElementById("screen").value=document.getElementById("screen").value.toString.slice(0,-1);
     }
     //hcf of two number
     function hcf(){
@@ -84,7 +90,7 @@ var screen=document.querySelector('#screen');
         if(a%i==0&b%i==0)
         gcd=i
         }
-        screen.value=gcd;
+        document.getElementById("screen").value=gcd;
             }
 //lcm of two no.
             function lcm(){
@@ -94,7 +100,7 @@ var screen=document.querySelector('#screen');
         break;
         }
         }
-        screen.value=i;
+        document.getElementById("screen").value=i;
             }
            //octal to decimal conversion 
          function octtodec(){
@@ -104,7 +110,7 @@ var screen=document.querySelector('#screen');
         k++;
         a=parseInt(a/10);
         }
-        screen.value=b;
+        document.getElementById("screen").value=b;
          }
 //decimal to binary conversion
          function dectobin(){
@@ -114,19 +120,19 @@ var screen=document.querySelector('#screen');
             k++;
             a=parseInt(a/2);
             }
-            screen.value=b;
+            document.getElementById("screen").value=b;
              }
 
 //binary to decimal conversion
          function bintodec(){
-        var a=screen.value,k=0,b=0;
+        var a=document.getElementById("screen").value,k=0,b=0;
         if(btntext==1||btntext==0){
         while(a){
         b=b+a%10*(2**k);
         k++;
         a=parseInt(a/10);
         }
-        screen.value=b;
+        document.getElementById("screen").value=b;
          }
         else{
         alert("reenter it is not valid");
@@ -135,13 +141,13 @@ var screen=document.querySelector('#screen');
         }
 //decimal to octal conversion
          function dectooct(){
-        var a=screen.value,k=0,b=0;
+        var a=document.getElementById("screen").value,k=0,b=0;
         while(a){
         b=b+a%8*(10**k);
         k++;
         a=parseInt(a/8);
         }
-        screen.value=b;
+        document.getElementById("screen").value=b;
          }
 //nth term of fibbonacci series
          function fibbo(){
@@ -151,11 +157,11 @@ var screen=document.querySelector('#screen');
         a=b;b=c;
         k++;
         }
-        screen.value=c;
+        document.getElementById("screen").value=c;
          }
 //nth term of ramanujan number
          function ramanujannum(){
-        var a=screen.value,t=0;
+        var a=document.getElementById("screen").value,t=0;
         for(i=1729;t<a;i++){
         k=0;
         for(x=1;x**3<i;x++){
@@ -165,14 +171,14 @@ var screen=document.querySelector('#screen');
         }
         }
         if(k==2){
-        screen.value=i;
+        document.getElementById("screen").value=i;
         t++;
         }
         }
          }
         //nth term of neilarmstrong number    
          function neilarmsnum(){
-        var a=screen.value,t=0;
+        var a=document.getElementById("screen").value,t=0;
         for(i=1;t<a;i++){
         nod=i.toString().length;
         sum=0;
@@ -184,21 +190,21 @@ var screen=document.querySelector('#screen');
         }
         if(sum==i){
         t++;
-        screen.value=i;
+        document.getElementById("screen").value=i;
         }
         }
          }
 //to check a number is prime or not
 function isprime(){
-        var a=parseInt(screen.value);
+        var a=parseInt(document.getElementById("screen").value);
         for(i=2;i<=a;i++){
         if(a%i==0)
         break;
         }
         if(a==i)
-        screen.value="prime number";
+        document.getElementById("screen").value="prime number";
         else
-        screen.value="not prime number";
+        document.getElementById("screen").value="not prime number";
          } 
           
             
